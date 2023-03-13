ebfd8251ab557416aa52aa5129ad82cd4ea0cb54
= <asdfghjkl.rs70@gmail.com>
2023-03-13 17:15:22 -0300

    update mpago webhook

--- pages/api/mpago-webhook.jsx
+++ pages/api/mpago-webhook.jsx
@@ -2,46 +2,50 @@ import axios from "axios";
 var mercadopago = require("mercadopago");
 
 export default async function handler(req, res) {
-  const subdomain = req.headers.host.split(".")[0];
+  if (req.body.type == "payment") {
+    const subdomain = req.headers.host.split(".")[0];
 
-  const franquia = await axios.post(
-    process.env.HASURA_URL,
-    {
-      query: `{
+    const franquia = await axios.post(
+      process.env.HASURA_URL,
+      {
+        query: `{
         franquia(where: {subdomain: {_eq: "${subdomain}"}}) {
           mpago_key
         }
        
       }`,
-    },
-    {
-      headers: {
-        "x-hasura-admin-secret": process.env.HASURA_ADMIN,
       },
-    }
-  );
-  mercadopago.configure({
-    access_token: franquia.data.data.franquia[0].mpago_key,
-  });
+      {
+        headers: {
+          "x-hasura-admin-secret": process.env.HASURA_ADMIN,
+        },
+      }
+    );
+    console.log(franquia.data);
+    mercadopago.configure({
+      access_token: franquia.data.data.franquia[0].mpago_key,
+    });
 
-  const orderStatus = await mercadopago.payment.get(req.body.data.id);
-  console.log(orderStatus);
-  const response = await axios.post(
-    process.env.HASURA_URL,
-    {
-      query: `mutation {
+    const orderStatus = await mercadopago.payment.get(req.body.data.id);
+    console.log(orderStatus);
+    const response = await axios.post(
+      process.env.HASURA_URL,
+      {
+        query: `mutation {
         update_user_carrinho(where: {mercado_order_id: {_eq: "${orderStatus.body.metadata.id}"}}, _set: {status: "${orderStatus.body.status}"}) {
           affected_rows
         }
        
       }`,
-    },
-    {
-      headers: {
-        "x-hasura-admin-secret": process.env.HASURA_ADMIN,
       },
-    }
-  );
+      {
+        headers: {
+          "x-hasura-admin-secret": process.env.HASURA_ADMIN,
+        },
+      }
+    );
 
-  res.status(200).json(response);
+    res.status(200).json(response);
+  }
+  res.status(200).json("oi");
 }
