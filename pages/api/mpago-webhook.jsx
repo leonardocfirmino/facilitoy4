440df0a38b9747b1337a6ebc60130bbb0fde2a65
= <asdfghjkl.rs70@gmail.com>
2023-03-13 16:59:54 -0300

    teste console.log

--- pages/api/mpago-webhook.jsx
+++ pages/api/mpago-webhook.jsx
@@ -23,7 +23,8 @@ export default async function handler(req, res) {
   mercadopago.configure({
     access_token: franquia.data.data.franquia[0].mpago_key,
   });
-  const orderStatus = await mercadopago.payment.get(req.body.data.id);
+  console.log(req.body);
+  const orderStatus = await mercadopago.payment.get(req.body.id);
   console.log(orderStatus);
   const response = await axios.post(
     process.env.HASURA_URL,
