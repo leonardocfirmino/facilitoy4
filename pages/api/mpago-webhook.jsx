157e17d551e4314c7d667be51d59a97b9dd64626
= <asdfghjkl.rs70@gmail.com>
2023-03-13 15:01:40 -0300

    teste preference id

--- pages/api/mpago-webhook.jsx
+++ pages/api/mpago-webhook.jsx
@@ -2,13 +2,11 @@ import axios from "axios";
 var mercadopago = require("mercadopago");
 
 export default async function handler(req, res) {
-  console.log(req.query);
-  console.log(req.body);
   const franquia = await axios.post(
     process.env.HASURA_URL,
     {
       query: `{
-        user_carrinho(where: {mercado_order_id: {_eq: "${req.body.data.id}"}}) {
+        user_carrinho(where: {mercado_order_id: {_eq: "${req.body.preference_id}"}}) {
          
           carrinho_produtos {
             product {
@@ -34,13 +32,13 @@ export default async function handler(req, res) {
       franquia.data.data.user_carrinho[0].carrinho_produtos[0].product.user
         .franquia[0].mpago_key,
   });
-  const orderStatus = await mercadopago.payment.get(req.body.data.id);
+  const orderStatus = await mercadopago.payment.get(req.body.preference_id);
 
   const response = await axios.post(
     process.env.HASURA_URL,
     {
       query: `mutation {
-        update_user_carrinho(where: {mercado_order_id: {_eq: "${req.body.data.id}"}}, _set: {status: "${orderStatus.body.status}"}) {
+        update_user_carrinho(where: {mercado_order_id: {_eq: "${req.body.preference_id}"}}, _set: {status: "${orderStatus.body.status}"}) {
           affected_rows
         }
        
