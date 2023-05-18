5aecc761faf53c41372fa89db7269b60b8b38f97
= <asdfghjkl.rs70@gmail.com>
2023-05-18 16:30:39 -0300

    google manager

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="YYD_AmLJ_z1heOFM0fkbPwDRwr1CmXpIsqdW1Cz3tBY"
+        />
         <title>Facilitoy</title>
       </Head>
       <body>
